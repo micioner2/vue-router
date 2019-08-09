f556b6f634ca2b7aa8842f8387c691d58c674a0c
micioner <mauricio_pella1@hotmail.com>
2019-08-08 22:26:19 -0500

    primer commit

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import(/* webpackChunkName: "about" */ './views/Home.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/servicio',
+      name: 'servicio',
+      component: () => import(/* webpackChunkName: "about" */ './views/Servicio.vue')
+    },
+  ]
+})
---
+++ src/views/Servicio.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="">
+        <h3>Servicio</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'servicio',
+
+
+}
+</script>
+
+<style>
+
+</style>
